edbdd1749a21a21557a67e1cda17f4a7a0394c38
JenBoardman <harter.art.creations@gmail.com>
2024-02-17 11:55:40 +0000

    added Github Repo URL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p className="signiture">
+        <a
+          href="https://github.com/JenBoardman/react-weather-app-final"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code on Github
+        </a>{" "}
+        by 🌙 Jennifer Boardman
+      </p>
+    </footer>
+  );
+}
